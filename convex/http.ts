87e9e799caaefe92542556d91709ae2780f10c9f
Quentin <Quentin.cheron26200@hotmail.com>
2026-02-15 14:31:00 +0100

    fix: import httpAction from _generated/server not convex/server

--- convex/http.ts
+++ convex/http.ts
@@ -1,5 +1,6 @@
-import { httpAction, httpRouter } from "convex/server";
+import { httpRouter } from "convex/server";
 import { internal } from "./_generated/api";
+import { httpAction } from "./_generated/server";
 import { authComponent, createAuth } from "./auth";
 
 const http = httpRouter();
