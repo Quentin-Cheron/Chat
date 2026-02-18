f888f88b4df1431c14486b16d7902a77e423351e
Quentin <Quentin.cheron26200@hotmail.com>
2026-02-18 22:24:29 +0100

    fix(install): remove unbound INSTALL_ROLE variable from final_output

--- install.sh
+++ install.sh
@@ -276,7 +276,6 @@ URL: https://$DOMAIN
 Admin: $ADMIN_EMAIL
 Mot de passe temporaire admin: $ADMIN_PASSWORD
 Invitation (MVP): https://$DOMAIN/invite/<code>
-Role: $INSTALL_ROLE
 Action requise: connectez-vous puis changez le mot de passe sur /security/change-password
 
 MinIO (object storage):
